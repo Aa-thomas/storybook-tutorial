57b12cc2fee78cb1de63fb2116bd52a0ea39dda5
Aaron Thomas <aarthomas01@gmail.com>
2023-06-07 12:37:12 -0400

    bgc blue

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -32,7 +32,7 @@ export default function Task({
 					readOnly={true}
 					name="title"
 					placeholder="Input title"
-					style={{ background: 'red' }}
+					style={{ background: 'blue' }}
 				/>
 			</label>
 
